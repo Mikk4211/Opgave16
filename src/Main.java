9118e05c8f4d5a3e6b97e3d3875fbaf628d16cd5
Mikkel Sørensen <mik4211@edu.easj.dk>
2017-09-15 10:30:46 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1+2+3+4+5+6+7+8+9+10");
+        // Her opstillet regnestykket med tekst.
+        System.out.println("     =");
+
+        System.out.println((1) + (2) + (3) + (4) + (5) + (6) + (7) + (8) + (9) + (10));
+        //Og her er den egentlige udregning, lavet med samme procedure som fra forrige opgave.
+        // Jeg har i regnestykket ikke brugt "" da det ikke bare er tekst der skal printes.
+    }
+}
